6f3dcb4dc75b4a471d8d0bfdc2a30b62f33eecef
Marten <marten.lug@gmail.com>
2021-11-19 17:13:03 +0100

    First changes

---
+++ HW6/src/exercise2/Bet_More_Game.java
@@ -0,0 +1,5 @@
+package exercise2;
+
+public class Bet_More_Game {
+
+}
---
+++ HW6/src/exercise2/Game_Chair.java
@@ -0,0 +1,12 @@
+package exercise2;
+
+public class Game_Chair {
+	
+	
+	public int Start(Player p) {
+		
+		
+		return 1;
+	}
+
+}
